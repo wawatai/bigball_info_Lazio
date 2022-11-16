3e9fec7a80d5c5a877e55b58eed0bb692d2814e4
wataiPublic@gmail.com <wataiPublic@gmail.com>
2022-11-16 16:25:51 +0800

    111-1116-4

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+//header
 $(function(){
     $("header .memberList").on("click",function(){
         $(this).find("ul")
@@ -10,14 +11,45 @@ $(function(){
     })
 })
 
+//mainArea高度
 $(function(){
     var th = $('.mainArea');
     var wh = window.innerHeight;
-    th.css("height", (String(wh - 335) + "px"));
+    th.css("height", (String(wh - 325) + "px"));
 
     $(window).resize(function () {
         var th = $('.mainArea');
         var wh = window.innerHeight;
-        th.css("height", (String(wh - 335) + "px"));
+        th.css("height", (String(wh - 325) + "px"));
     });
 });
+
+//mainArea table展開
+$(function(){
+    $(".mainArea .tableList .title").on("click",function(){
+        $(this).closest(".listBox")
+        .toggleClass("active");
+    })
+
+    $(".mainArea .topBtnBox button").on("click",function(){
+        if($(".mainArea .tableList .listBox").hasClass("active"))
+        {
+            $(".mainArea .tableList .listBox")
+            .removeClass("active");
+        }
+        else
+        {
+            $(".mainArea .tableList .listBox")
+            .addClass("active");
+        }
+    })
+})
+
+//nav
+$(function(){
+    $("nav li").on("click",function(){
+        $(this)
+        .addClass("active")
+        .siblings().removeClass("active");
+    })
+})
